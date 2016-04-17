7d742e079165ef8ee8067a454110f8d5f047f6d7
Kento Okamoto <oka.keno@gmail.com>
2016-04-16 20:58:19 -0600

    Video displaying on output image

--- src/main.cpp
+++ src/main.cpp
@@ -15,11 +15,11 @@ using namespace std;
 int main(int argv, char* argc[]){
 
 	printf("Hit ESC key to quit\n");
-	cv::VideoCapture cap("Photos/IMG_1284.MOV ");
-
+	cv::VideoCapture cap("./src/Photos/IMG_1284.MOV");
+	//cv::VideoCapture cap(0);
 	if (!cap.isOpened()) {          // check if we succeeded
-		printf("error - can't open the camera; hit any key to quit\n");
-		system("PAUSE");
+		printf("error - can't open the video file; hit enter key to quit\n");
+		cin.ignore().get();
 		return EXIT_FAILURE;
 	}
 
@@ -29,9 +29,13 @@ int main(int argv, char* argc[]){
 		if (imageInput.empty()) break;
 
 		cv::Mat imageInputGray;
-		cv::cvtColor(imageInput, imageInputGray, cv::COLOR_BGR2GRAY);
-
+		cv::cvtColor(imageInput, imageInputGray, cv::COLOR_BGR2GRAY);cv::namedWindow("My Image", cv::WINDOW_AUTOSIZE );
+		cv::resize(imageInputGray, imageInputGray, cv::Size(imageInputGray.cols/4, imageInputGray.rows/4));	
+		cv::namedWindow("My Image", cv::WINDOW_NORMAL);
 		cv::imshow("Marker", imageInputGray);
+
+		if (cv::waitKey(1) == 27)  break;  // hit ESC (ascii code 27) to quit
+
 	}
 	return EXIT_SUCCESS;
 }
